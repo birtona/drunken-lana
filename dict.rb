08ea7b1745417a880e6768a73b01599079cda1f9
birtona <birte@git.de>
2012-06-29 13:20:37 +0200

    wörter ergänzt

---
+++ dict.rb
@@ -0,0 +1,13 @@
+dict_hash = {}
+
+dict_hash['nuna'] = 'Schnuller'
+dict_hash['miech'] = 'Milch'
+dict_hash['ajo'] = 'Hallo'
+dict_hash['audo'] = 'Auto'
+dict_hash['bagga'] = 'Bagger'
+dict_hash['awowo'] = 'Hund'
+dict_hash['ei'] = 'Katze'
+
+dict_hash.each do |l_word, word|
+puts l_word + ': ' + word
+end  
